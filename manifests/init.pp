657f705f7ecc53549a68361484a0c726758b98f0
Kit Plummer <kitplummer@gmail.com>
2013-06-19 16:25:03 -0700

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+# Install Oracle JDK.
+class oraclejdk {
+	include oraclejdk::packages
+	include oraclejdk::services
+	include oraclejdk::config
+
+  $jdk_url = "https://dl.dropboxusercontent.com/u/2219585/jdk-7u25-linux-x64.rpm"
+
+  package { 'wget': ensure => installed }
+
+  exec { 'download-jdk':
+    require => Package['wget'],
+    command => "/usr/bin/wget -O /var/tmp/jdk.rpm $jdk_url --no-check-certificate",
+    creates => "/var/tmp/jdk.rpm",
+  }
+
+  exec { 'install-jdk':
+    require => Exec['download-jdk'],
+    command => '/usr/bin/yum -y -d0 install /var/tmp/jdk.rpm',
+    unless  => '/bin/rpm -q jdk',
+  }
+
+  file { '/etc/profile.d':
+    source  => 'puppet:///modules/oraclejdk/profile.d',
+    recurse => true,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+
+}
+
